f9c51087e8648587750fdccf6ba5b613c3e71136
Farha Islam <farha.islam@students.codeinstitute.net>
2024-07-20 04:43:48 +0000

    Set debug to True

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-831^68d+l=b8&&f2+kjha&lf4q4j-!h0@%lrr^31qlsg-8h&l7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=False
+DEBUG=True
 
 ALLOWED_HOSTS = ['8000-farhatamann-djangoproje-iqqtvw6106f.ws.codeinstitute-ide.net','.herokuapp.com']
 
